c6caa1fd8eb5ceba9b362780ab2eafcff08fb81a
805eliasmello <149224437+805eliasmello@users.noreply.github.com>
2024-11-25 06:10:11 -0300

    nova att

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const buttons = document.querySelectorAll('.button');
-    
-    if (buttons.length > 0) {
-        buttons.forEach(button => {
-            button.addEventListener('click', () => {
-                alert('Esta funcionalidade estará disponível em breve!');
-            });
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Esta funcionalidade estará disponível em breve!');
         });
-    } else {
-        console.warn('Nenhum botão encontrado com a classe .button');
-    }
+    });
 });
